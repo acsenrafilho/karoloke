fab9c59cf52d442bf8e5eadb2bd37dbaa02ac781
Antonio Senra <acsenrafilho@gmail.com>
2025-07-12 08:36:25 -0300

    ENH: Implement karaoke framework with jukebox functionality, including background image handling, video file management, and user interface templates

---
+++ karoloke/start_karaoke.py
@@ -0,0 +1,9 @@
+import os
+from jukebox_router import app
+from settings import BACKGROUND_DIR, VIDEO_DIR
+
+if __name__ == '__main__':
+    # Ensure video and backgrounds folders exist
+    os.makedirs(BACKGROUND_DIR, exist_ok=True)
+    os.makedirs(VIDEO_DIR, exist_ok=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ tests/test_jukebox_controller.py
@@ -0,0 +1,17 @@
+import pytest
+import os
+import random
+
+from karoloke.jukebox_controller import get_background_img
+import pathlib
+
+BACKGROUND_FOLDER = os.path.join(pathlib.Path(__file__).parents[1], 'karoloke','backgrounds')
+
+def test_get_background_img_raise_error_for_empty_image_list(tmpdir):
+    with pytest.raises(FileNotFoundError, match="No background images found in the 'background' directory."):
+        get_background_img(background_dir=tmpdir)
+
+def test_get_background_img_returns_image():
+    # Call the function and check if it returns the correct image
+    result = get_background_img(background_dir=str(BACKGROUND_FOLDER))
+    assert len(result) > 0 # type: ignore
