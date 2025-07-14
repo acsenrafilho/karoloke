98c0a2b44ae688af555287e893a5a993f3f492d0
Antonio Senra <acsenrafilho@gmail.com>
2025-07-13 23:07:03 -0300

    Initialize singers dictionary in settings

--- karoloke/settings.py
+++ karoloke/settings.py
@@ -8,3 +8,6 @@ BACKGROUND_DIR = os.path.join(os.path.dirname(__file__), 'backgrounds')
 VIDEO_DIR = os.path.join(os.path.dirname(__file__), 'videos')
 PLAYER_TEMPLATE = 'player.html'
 VIDEO_PATH_SETUP_TEMPLATE = 'video_path_setup.html'
+
+# Set the singers dictionary to an empty dictionary
+SINGERS = {}
